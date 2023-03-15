fb795edb43ea2780c6367ece947f5d357396e9ed
dandeliondino <68911895+dandeliondino@users.noreply.github.com>
2023-03-15 09:15:02 -0700

    adjust inspector section button margins

--- addons/tile_bit_tools/controls/shared/inspector_section_button.gd
+++ addons/tile_bit_tools/controls/shared/inspector_section_button.gd
@@ -2,6 +2,8 @@
 extends Button
 
 
+const LABEL_MARGIN_ADJUST := -6
+
 const TBTPlugin := preload("res://addons/tile_bit_tools/controls/tbt_plugin_control/tbt_plugin_control.gd")
 
 
@@ -26,8 +28,9 @@ var hover_color : Color
 @onready var background_rect: ColorRect = %BackgroundRect
 @onready var arrow_rect: TextureRect = %ArrowRect
 @onready var label: Label = %Label
-@onready var left_margin_control: Control = %LeftMarginControl
-@onready var h_box_container: HBoxContainer = %HBoxContainer
+@onready var arrow_margin_container: MarginContainer = $ArrowMarginContainer
+@onready var label_margin_container: MarginContainer = $LabelMarginContainer
+
 
 
 func _ready() -> void:
@@ -41,16 +44,19 @@ func _tbt_ready() -> void:
 	
 	icon_expanded = tbt.icons.get_icon(tbt.icons.ARROW_EXPANDED)
 	icon_collapsed = tbt.icons.get_icon(tbt.icons.ARROW_COLLAPSED)
+	_toggle_expanded(false)
 	
 	normal_color = tbt.base_control.get_theme_color("prop_subsection", "Editor")
 	normal_color.a = normal_color.a * 0.4
 	hover_color = normal_color.lightened(0.2)
-	
-	left_margin_control.custom_minimum_size.x = round(2 * tbt.interface.get_editor_scale())
-	h_box_container.set("theme_override_constants/separation", 0)
-	
 	_toggle_hover(false)
-	_toggle_expanded(false)
+	
+	var left_margin = round(2 * tbt.interface.get_editor_scale())
+	arrow_margin_container.set("theme_override_constants/margin_left", left_margin)
+	
+	var label_margin = left_margin + icon_expanded.get_size().x + LABEL_MARGIN_ADJUST
+	label_margin_container.set("theme_override_constants/margin_left", label_margin)
+
 
 
 func _update_label_text() -> void:
@@ -83,5 +89,5 @@ func _on_mouse_exited() -> void:
 	_toggle_hover(false)
 
 
-func _on_toggled(button_pressed: bool) -> void:
-	_toggle_expanded(button_pressed)
+func _on_toggled(p_button_pressed: bool) -> void:
+	_toggle_expanded(p_button_pressed)
